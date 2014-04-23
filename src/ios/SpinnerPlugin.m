303bbe84544b18d23a3b36db3feda6f49f4f5177
Modern Alchemists <modern@Moderns-MacBook-Pro.local>
2014-04-23 13:41:08 +0200

    Fixed positionRemoved "background" UIView (indicator already has a background, no need to add one) Fixed indicator positioning (now it takes the screen orientation into account) showOverlay now defaults to true instead of false (so now it´s equal to the Android Plugin behaviour)

--- src/ios/SpinnerPlugin.m
+++ src/ios/SpinnerPlugin.m
@@ -4,6 +4,7 @@
 //
 //  Created by Andrea Maschio on 24/12/13.
 //
+//  Updated by Modern Alchemists OG on 23/04/2014.
 //
 
 #import "SpinnerPlugin.h"
@@ -12,83 +13,107 @@
 #define PARAM_AUTO_HIDE_TIMEOUT @"timeout"
 
 @implementation SpinnerPlugin
+
 - (void)pluginInitialize{
     //params init
     showOverlay = YES;
     connectionTimeout = 0;
 }
--(void)show:(CDVInvokedUrlCommand*)options{
+
+-(void)show:(CDVInvokedUrlCommand*)options
+{
     NSMutableDictionary* params = [options.arguments objectAtIndex:0];
     
-    ;
-    
-    if([params objectForKey:PARAM_AUTO_HIDE_TIMEOUT]){
+    // Parse parameters
+    if([params objectForKey:PARAM_AUTO_HIDE_TIMEOUT])
+    {
         id connectiontimeout =[params objectForKey:PARAM_AUTO_HIDE_TIMEOUT];
         
-        if([connectiontimeout isKindOfClass:[NSString class]]){
-            NSScanner*scan = [NSScanner scannerWithString:(NSString*)connectiontimeout];
+        if([connectiontimeout isKindOfClass:[NSString class]])
+        {
+            NSScanner* scan = [NSScanner scannerWithString:(NSString*)connectiontimeout];
             [scan scanInt:&connectionTimeout];
-        }else if([connectiontimeout isKindOfClass:[NSNumber class]]){
+        }
+        else if( [connectiontimeout isKindOfClass:[NSNumber class]] )
+        {
             connectionTimeout = [connectiontimeout intValue];
         }
     }
     
-    
     id overlayParam = [params objectForKey:PARAM_SHOW_OVERLAY];
-    
     if ([overlayParam isKindOfClass:[NSString class]])
     {
-        if ([overlayParam isEqualToString:@"false"])
+        if( [overlayParam isEqualToString:@"false"] )
+        {
             showOverlay = NO;
-    }
-    else if (![overlayParam boolValue])
-        showOverlay = NO;
-    
-    if(!indicator){
-        indicator = [[UIActivityIndicatorView alloc]initWithFrame:[UIScreen  mainScreen].bounds];
-        [indicator setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleWhiteLarge];
-        
-    }else{
-        [indicator removeFromSuperview];
-    }
-    if(showOverlay){
-        if(!background){
-            background = [[UIView alloc]initWithFrame:[UIScreen  mainScreen].bounds];
-        }else{
-            [background removeFromSuperview];
         }
     }
+    else if (![overlayParam boolValue])
+    {
+        showOverlay = YES; // showOverlay defaults to YES if undefined
+    }
+        
+    if(!indicator)
+    {
+        indicator = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+        
+        // center indicator and resize/colorize frame (take screen orientation into account)
+        CGRect frame = indicator.frame;
+        UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+        frame.origin.x = 0;
+        frame.origin.y = 0;
+        if( orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight )
+        {
+            // landscape
+            frame.size.width = self.viewController.view.frame.size.height;
+            frame.size.height = self.viewController.view.frame.size.width;
+        }
+        else
+        {
+            // portrait
+            frame.size.width = self.viewController.view.frame.size.width;
+            frame.size.height = self.viewController.view.frame.size.height;
+        }
+        indicator.frame = frame;
+        indicator.layer.cornerRadius = 0;
+        indicator.opaque = YES;
+        indicator.backgroundColor = [UIColor colorWithWhite:0.0f alpha:0.0f];
+    }
+    else
+    {
+        [indicator removeFromSuperview];
+    }
     
-    if(connectionTimeout>0){
+    if( showOverlay )
+    {
+        indicator.backgroundColor = [UIColor colorWithWhite:0.0f alpha:0.6f];
+    }
+    
+    if( connectionTimeout > 0 )
+    {
         NSTimer *timer __attribute__((unused))= [NSTimer scheduledTimerWithTimeInterval:connectionTimeout target:self selector:@selector(hideAfterTimeout:) userInfo:Nil repeats:NO];
     }
-    [self.viewController.view addSubview:background];
-    
-    
-    [background setBackgroundColor:[UIColor blackColor]];
-    [background setAlpha:0.4f];
-    background.userInteractionEnabled=NO;
     
     [self.viewController.view addSubview:indicator];
-[indicator startAnimating];
+    [indicator startAnimating];
 }
--(void)hideAfterTimeout:(NSTimer*)timer{
-    //NSLog(@"Hiding");
+
+-(void)hideAfterTimeout:(NSTimer*)timer
+{
     [timer invalidate];
     [self hide:nil];
 }
--(void)hide:(CDVInvokedUrlCommand*)options{
-    //NSLog(@"Hiding2");
-    [indicator stopAnimating];
-    [background removeFromSuperview];
-    [indicator removeFromSuperview];
-    
 
+-(void)hide:(CDVInvokedUrlCommand*)options
+{
+    [indicator stopAnimating];
+    [indicator removeFromSuperview];
 }
--(void)dispose{
+
+-(void)dispose
+{
     [super dispose];
-    //NSLog(@"disposing plugin");
     indicator=nil;
-    background = nil;
 }
+
 @end
